bc90af66012d4d691b86b51ae808224d4915c65a
김진원 <48236565+Dohamsu@users.noreply.github.com>
2024-05-07 11:44:32 +0900

    지휘자 파트 추가, 닉네임 수정 추가

--- src/services/EventRegistrationService.ts
+++ src/services/EventRegistrationService.ts
@@ -7,27 +7,25 @@ interface EventData {
   place: string;
   description: string;
   type: string;
-  startDate: Dayjs;  // Dayjs 객체
-  startTime: Dayjs;  // Dayjs 객체
-  endTime: Dayjs;    // Dayjs 객체
+  startDate: Dayjs;
+  startTime: Dayjs;
+  endTime: Dayjs;
+  scheduleNumber?: string;  // 'id' 대신 'scheduleNumber' 속성을 사용합니다.
 }
 
 export const registerEvent = async (eventData: EventData): Promise<void> => {
   const { title, place, description, type, startDate, startTime, endTime } = eventData;
-
-  // Dayjs 객체를 ISO 문자열로 변환
   const formattedEventData = {
     title,
     place,
     description,
     type,
-    startDate: startDate.toISOString(),  // ISO 문자열로 변환
-    startTime: startTime.toISOString(),  // ISO 문자열로 변환
-    endTime: endTime.toISOString(),      // ISO 문자열로 변환
+    startDate: startDate.toISOString(),
+    startTime: startTime.toISOString(),
+    endTime: endTime.toISOString(),
   };
 
   const POST_SCHEDULE_API_URL = `${process.env.REACT_APP_API_SERVER_URI}/api/schedule`;
-
 
   try {
     const response = await axios.post(POST_SCHEDULE_API_URL, formattedEventData);
@@ -36,7 +34,33 @@ export const registerEvent = async (eventData: EventData): Promise<void> => {
   } catch (error) {
     console.error('Failed to register event:', error);
     alert('일정 등록에 문제가 발생했습니다. 잠시 후 다시 시도해주세요.');
+    throw error;
+  }
+};
 
-    throw error; // Optionally re-throw to handle it differently in the UI
+export const updateEvent = async (eventData: EventData): Promise<void> => {
+  const { scheduleNumber, title, place, description, type, startDate, startTime, endTime } = eventData;
+  if (!scheduleNumber) throw new Error('Schedule number is required for updating an event.');
+
+  const formattedEventData = {
+    title,
+    place,
+    description,
+    type,
+    startDate: startDate.toISOString(),
+    startTime: startTime.toISOString(),
+    endTime: endTime.toISOString(),
+  };
+
+  const PUT_SCHEDULE_API_URL = `${process.env.REACT_APP_API_SERVER_URI}/api/schedule/${scheduleNumber}`;
+
+  try {
+    const response = await axios.put(PUT_SCHEDULE_API_URL, formattedEventData);
+    console.log('Event updated successfully:', response.data);
+    alert('일정이 정상적으로 수정되었습니다.');
+  } catch (error) {
+    console.error('Failed to update event:', error);
+    alert('일정 수정에 문제가 발생했습니다. 잠시 후 다시 시도해주세요.');
+    throw error;
   }
 };
