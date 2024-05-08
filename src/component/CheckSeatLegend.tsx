671e7515c59cfd8de3859e9e6f1580d79090dbe1
김진원 <48236565+Dohamsu@users.noreply.github.com>
2024-05-08 09:27:47 +0900

    css 수정

--- src/component/CheckSeatLegend.tsx
+++ src/component/CheckSeatLegend.tsx
@@ -6,27 +6,28 @@ const CheckSeatLegend: React.FC = () => {
   return (
     <Box sx={{ 
       position: 'absolute',
-      top: '90%',
+      top: '91%',
       display: 'flex',
       justifyContent: 'center',
+      fontSize:'0.8rem !important',
       gap: '20px',
       width:'100%' }}>
       {/* 범례 항목을 배열로 만들어서 map을 사용하여 표시할 수 있습니다. */}
       <Box sx={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-        <Box sx={{ width: '20px', height: '20px', backgroundColor: '#00b38360'}} />
+        <Box sx={{ width: '15px', height: '15px', backgroundColor: '#00b38360'}} />
         <Typography>출석 예정</Typography>
       </Box>
       <Box sx={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-        <Box sx={{ width: '20px', height: '20px', backgroundColor: '#00b383' }} />
+        <Box sx={{ width: '15px', height: '15px', backgroundColor: '#00b383' }} />
         <Typography>출석 완료</Typography>
       </Box>
       <Box sx={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-        <Box sx={{ width: '20px', height: '20px', backgroundColor: '#FF6347' }} />
+        <Box sx={{ width: '15px', height: '15px', backgroundColor: '#FF6347' }} />
         <Typography>불참</Typography>
       </Box>
     
       <Box sx={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-        <Box sx={{ width: '20px', height: '20px', backgroundColor: '#FFA500' }} />
+        <Box sx={{ width: '15px', height: '15px', backgroundColor: '#FFA500' }} />
         <Typography>미정</Typography>
       </Box>
     </Box>
