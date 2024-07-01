8188b18bb3f19f364dec32282304e114a7a5df09
김진원 <48236565+Dohamsu@users.noreply.github.com>
2024-07-01 13:44:47 +0900

    전체 일정 보기, 미래 일정 보기 추가

--- src/component/ScheduleBox.tsx
+++ src/component/ScheduleBox.tsx
@@ -11,13 +11,16 @@ import IconClockPerAdd from "../images/scheduleBoxCircle3.svg";
 import moreIconUrl from "../images/scheduleMoreUrl.svg";
 import { Schedule, ScheduleBoxProps } from '../stores/type';
 import { fetchSchedules, updateAttendance } from '../common/scheduleService';
+import { Box } from '@mui/material';
 
-const ScheduleBox: React.FC<ScheduleBoxProps> = ({ schedules, setSchedules, selectedEvents, setSelectedEvents, month, day }) => {
+const ScheduleBox: React.FC<ScheduleBoxProps & { showPastEvents: boolean }> = ({ schedules, setSchedules, selectedEvents, setSelectedEvents, oneDayList, showPastEvents }) => {
+  console.log(selectedEvents);
   const userInfo = useSelector((state: RootState) => state.user.user);
   const [editScheduleData, setEditScheduleData] = useState<Schedule | null>(null);
-
   const [openOption, setOpenOption] = useState<string | null>(null);
   const [showPopup, setShowPopup] = useState<boolean>(false);
+  const [filteredSchedules, setFilteredSchedules] = useState<Schedule[]>([]);
+
   const popupRef = useRef<HTMLDivElement>(null);
 
   interface EventData {
@@ -45,7 +48,17 @@ const ScheduleBox: React.FC<ScheduleBoxProps> = ({ schedules, setSchedules, sele
   }, [openOption]);
 
   useEffect(() => {
+    let newFilteredSchedules = schedules;
+
     if (selectedEvents.length > 0) {
+      newFilteredSchedules = selectedEvents;
+    } else if (!showPastEvents) {
+      newFilteredSchedules = schedules.filter(schedule => dayjs(schedule.startDate).isSameOrAfter(dayjs(), 'day'));
+    }
+
+    setFilteredSchedules(newFilteredSchedules);
+
+    if (selectedEvents.length > 0 || showPastEvents) {
       let hasChanges = false;
       const newSchedules = schedules.map(schedule => {
         const foundEvent = selectedEvents.find(event => event.scheduleNumber === schedule.scheduleNumber);
@@ -60,47 +73,47 @@ const ScheduleBox: React.FC<ScheduleBoxProps> = ({ schedules, setSchedules, sele
         setSchedules(newSchedules);
       }
     }
-  }, [selectedEvents]);
-  
+  }, [selectedEvents, showPastEvents, schedules, setSchedules]);
+
   const handleMoreIconClick = useCallback((scheduleNumber: string) => {
     setOpenOption(prev => prev === scheduleNumber ? null : scheduleNumber);
   }, []);
-  
+
   const toggleAttendance = useCallback(async (scheduleNumber: string, status: number) => {
     if (!userInfo?.name) return;
     setOpenOption(null);
-  
+
     try {
       await updateAttendance(scheduleNumber, userInfo.name, status);
-      const updatedSchedules = schedules.map(schedule => 
+      const updatedSchedules = schedules.map(schedule =>
         schedule.scheduleNumber === scheduleNumber ? { ...schedule, isAttending: status } : schedule
       );
       setSchedules(updatedSchedules);
-  
-      const updatedSelectedEvents = selectedEvents.map(event => 
+
+      const updatedSelectedEvents = selectedEvents.map(event =>
         event.scheduleNumber === scheduleNumber ? { ...event, isAttending: status } : event
       );
       setSelectedEvents(updatedSelectedEvents);
-  
+
     } catch (error) {
       console.error('Error updating attendance:', error);
     }
   }, [userInfo, schedules, setSchedules, selectedEvents, setSelectedEvents]);
-  
-// 타입 변환 함수 예시
-function convertScheduleToEventData(schedule: Schedule | null): EventData {
-  return {
-    title: schedule?.title,
-    place: schedule?.place,
-    description: schedule?.description,
-    type: schedule?.type,
-    startDate: schedule?.startDate, // Dayjs 객체를 예상하는 필드
-    startTime: schedule?.startTime,
-    endTime: schedule?.endTime,
-    scheduleNumber: schedule?.scheduleNumber,
-    // 추가적으로 필요한 필드들을 매핑하거나 초기화
-  };
-}
+
+  // 타입 변환 함수 예시
+  function convertScheduleToEventData(schedule: Schedule | null): EventData {
+    return {
+      title: schedule?.title,
+      place: schedule?.place,
+      description: schedule?.description,
+      type: schedule?.type,
+      startDate: schedule?.startDate, // Dayjs 객체를 예상하는 필드
+      startTime: schedule?.startTime,
+      endTime: schedule?.endTime,
+      scheduleNumber: schedule?.scheduleNumber,
+      // 추가적으로 필요한 필드들을 매핑하거나 초기화
+    };
+  }
 
   // 일정 수정 버튼 이벤트 핸들러
   const handleEditSchedule = useCallback((schedule: Schedule) => {
@@ -109,8 +122,31 @@ function convertScheduleToEventData(schedule: Schedule | null): EventData {
   }, []);
 
   return (
-    <div className="schedule-container">
-       {showPopup && (
+    <Box
+      className="schedule-container"
+      height='70%'
+      overflow='scroll'
+      sx={{
+        overflowX: 'hidden',
+        position: 'relative',
+        '&::-webkit-scrollbar': {
+          width: '5px',
+          borderRadius: '4px',
+        },
+        '&::-webkit-scrollbar-track': {
+          background: '#f1f1f1',
+          borderRadius: '4px',
+        },
+        '&::-webkit-scrollbar-thumb': {
+          background: '#888',
+          borderRadius: '4px',
+        },
+        '&::-webkit-scrollbar-thumb:hover': {
+          background: '#555',
+        }
+      }}
+    >
+      {showPopup && userInfo?.isAdmin && (
         <ScheduleRegiPopup
           isUpdate={true}
           isVisible={showPopup}
@@ -121,15 +157,15 @@ function convertScheduleToEventData(schedule: Schedule | null): EventData {
           }}
         />
       )}
-      {schedules.map((schedule, index) => (
+      {filteredSchedules.map((schedule, index) => (
         <article key={index} className="task-card">
           <img src={
-            schedule.type === "연습" ? IconClockPerMain :
-            schedule.type === "일정" ? IconClockPerSub : IconClockPerAdd
+            schedule.type === "재학생 연습" ? IconClockPerMain :
+              schedule.type === "전체 연습" ? IconClockPerSub : IconClockPerAdd
           } alt="" className="scheduleBoxCircle" />
-         <div className="time-container">
-            <time className="time">{dayjs(schedule.startDate).format('YYYY-MM-DD') + " / "+ dayjs(schedule.startTime).format('HH:mm') + ' ~ ' 
-            +dayjs(schedule.endTime).format('HH:mm') }</time>
+          <div className="time-container">
+            <time className="time">{dayjs(schedule.startDate).format('YYYY-MM-DD') + " / " + dayjs(schedule.startTime).format('HH:mm') + ' ~ '
+              + dayjs(schedule.endTime).format('HH:mm')}</time>
           </div>
           <img src={moreIconUrl} alt="more options" className="more-icon"
             onClick={() => handleMoreIconClick(schedule.scheduleNumber)} />
@@ -138,30 +174,26 @@ function convertScheduleToEventData(schedule: Schedule | null): EventData {
               <button onClick={() => toggleAttendance(schedule.scheduleNumber, 0)} className="option-button">미정</button>
               <button onClick={() => toggleAttendance(schedule.scheduleNumber, 1)} className="option-button">출석예정</button>
               <button onClick={() => toggleAttendance(schedule.scheduleNumber, 3)} className="option-button">불참</button>
-              {/* {userInfo?.isAdmin && (
-                <button onClick={() => handleEditSchedule(schedule)} className="option-button">일정수정</button>
-              )} */}
             </div>
           )}
           <h3 className="title" onClick={() => handleEditSchedule(schedule)}>{schedule.title}</h3>
           <p className="description">{schedule.place}</p>
           <div className={`attendance-status ${
-             schedule.isAttending === 0 ? 'pending' :
-            schedule.isAttending === 1 ? 'attending' :
-            schedule.isAttending === 2 ? 'attended' :
-            schedule.isAttending === 3 ? 'not-attending' :
-            'pending'}`}>
+            schedule.isAttending === 0 ? 'pending' :
+              schedule.isAttending === 1 ? 'attending' :
+                schedule.isAttending === 2 ? 'attended' :
+                  schedule.isAttending === 3 ? 'not-attending' :
+                    'pending'}`}>
 
             {schedule.isAttending === 0 ? '미정' :
-            schedule.isAttending === 1 ? '출석 예정' :
-            schedule.isAttending === 2 ? '출석 완료' :
-            schedule.isAttending === 3 ? '불참' :
-            '미정'}
+              schedule.isAttending === 1 ? '출석 예정' :
+                schedule.isAttending === 2 ? '출석 완료' :
+                  schedule.isAttending === 3 ? '불참' :
+                    '미정'}
           </div>
-
         </article>
       ))}
-    </div>
+    </Box>
   );
 };
 
--- src/screen/CalendarPage.tsx
+++ src/screen/CalendarPage.tsx
@@ -2,15 +2,17 @@ import React, { useState, useEffect } from "react";
 import "../css/calendarPage.css";
 import { useSelector } from 'react-redux';
 import { RootState } from '../stores/store';
-import {Calendar} from '../component/Calendar'
-import  ScheduleBox  from "../component/ScheduleBox";
+import { Calendar } from '../component/Calendar';
+import ScheduleBox from "../component/ScheduleBox";
 import { Schedule } from '../stores/type';
-import {fetchSchedules} from '../common/scheduleService';
+import { fetchSchedules } from '../common/scheduleService';
 import NoticePopup from "../component/NoticePopup";
+import { Box, Button } from '@mui/material';
 
 const CalendarPage = () => {
   const [schedules, setSchedules] = useState<Schedule[]>([]);
   const [selectedEvents, setSelectedEvents] = useState<Schedule[]>([]);  //당일 일정이 담긴 목록
+  const [showPastEvents, setShowPastEvents] = useState<boolean>(false); // 이전 일정 표시 여부
 
   const userInfo = useSelector((state: RootState) => state.user.user);
   const GET_SCHEDULE_API_URL = `${process.env.REACT_APP_API_SERVER_URI}/api/schedule`;
@@ -25,14 +27,30 @@ const CalendarPage = () => {
 
   return (
     <div className="calendar-container">
-        <NoticePopup apiEndpoint={GET_SCHEDULE_API_URL} title={''}/>
-
+      <NoticePopup apiEndpoint={GET_SCHEDULE_API_URL} title={''} />
       <div className="calendar-item">
-        <Calendar month={0}day={0} schedules={schedules} selectedEvents={selectedEvents}  setSelectedEvents={setSelectedEvents} setSchedules={setSchedules}  />
+        <Calendar month={0} day={0} schedules={schedules} selectedEvents={selectedEvents} setSelectedEvents={setSelectedEvents} setSchedules={setSchedules} oneDayList={true} />
       </div>
       <div className="calendar-item">
-          <h2>전체 일정</h2>
-          <ScheduleBox month={0}  day={0} schedules={schedules} setSchedules={setSchedules}  selectedEvents={selectedEvents} setSelectedEvents={setSelectedEvents}/>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '0px' }}>
+          <h2>일정</h2>
+          <Button
+            onClick={() => setShowPastEvents((prev) => !prev)}
+            sx={{marginRight:'10px'}}
+          >
+            {showPastEvents ? "미래 일정만 보기" : "전체 일정 보기"}
+          </Button>
+        </Box>
+        <ScheduleBox
+          month={0}
+          day={0}
+          oneDayList={false}
+          schedules={schedules}
+          setSchedules={setSchedules}
+          selectedEvents={[]}
+          setSelectedEvents={setSelectedEvents}
+          showPastEvents={showPastEvents} // showPastEvents 상태 전달
+        />
       </div>
     </div>
   );
