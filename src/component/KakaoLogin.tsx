0f83a5417fd133057e8e4f11a2805828db672687
김진원 <48236565+Dohamsu@users.noreply.github.com>
2024-07-04 14:32:58 +0900

    카카오 자동로그인 보완

--- src/component/KakaoLogin.tsx
+++ src/component/KakaoLogin.tsx
@@ -1,27 +1,27 @@
-// kakaoLogin.tsx
 import React, { useState, useEffect } from 'react';
 import kakaoLoginImage from '../images/kakao_login.png';
 import CONST from '../common/const';
 import { useDispatch } from 'react-redux';
 import { Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
-import axios, { AxiosError } from 'axios';
+import axios from 'axios';
 import { login, logoutUser, setLoginChecked } from '../stores/userSlice';
-import { AppDispatch } from '../stores/store'; // AppDispatch 타입 import
+import { AppDispatch } from '../stores/store';
 
 const AutoKakaoLoginCheck: React.FC = () => {
-  const dispatch = useDispatch<AppDispatch>(); // 여기에 AppDispatch 타입을 사용
+  const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
     const accessToken = localStorage.getItem('kakao_token');
+    const refreshToken = localStorage.getItem('kakao_refresh_token');
     if (accessToken) {
-      verifyToken(accessToken);
+      verifyToken(accessToken, refreshToken);
     } else {
       dispatch(logoutUser());
       dispatch(setLoginChecked(true));
     }
   }, [dispatch]);
 
-  const verifyToken = async (token: string) => {
+  const verifyToken = async (token: string, refreshToken: string | null) => {
     try {
       const response = await axios.get('https://kapi.kakao.com/v2/user/me', {
         headers: {
@@ -43,48 +43,111 @@ const AutoKakaoLoginCheck: React.FC = () => {
         };
         dispatch(login(user));
 
-        console.log(response);
-        localStorage.setItem('kakao_res', response.data);
-        localStorage.setItem('kakao_token', response.data.access_token);
+        localStorage.setItem('kakao_res', JSON.stringify(response.data));
+        localStorage.setItem('kakao_token', token);
         localStorage.setItem('last_login_platform', 'kakao');
-
-        console.log('카카오 자동로그인 성공');
       } else {
         throw new Error('Token verification failed');
       }
     } catch (error) {
       console.error('Error verifying token:', error);
-      localStorage.removeItem('kakao_token');
-      dispatch(logoutUser());
+      if (refreshToken) {
+        await refreshAccessToken(refreshToken);
+      } else {
+        localStorage.removeItem('kakao_token');
+        dispatch(logoutUser());
+        dispatch(setLoginChecked(true));
+      }
     }
-    dispatch(setLoginChecked(true));
+  };
+
+  const refreshAccessToken = async (refreshToken: string) => {
+    try {
+      const response = await axios.post('https://kauth.kakao.com/oauth/token', null, {
+        params: {
+          grant_type: 'refresh_token',
+          client_id: CONST.KAKAO_REST_API_KEY,
+          refresh_token: refreshToken,
+        },
+        headers: {
+          'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+        },
+      });
+
+      if (response.status === 200) {
+        const newAccessToken = response.data.access_token;
+        localStorage.setItem('kakao_token', newAccessToken);
+        verifyToken(newAccessToken, refreshToken);
+      } else {
+        throw new Error('Failed to refresh access token');
+      }
+    } catch (error) {
+      console.error('Error refreshing access token:', error);
+      localStorage.removeItem('kakao_token');
+      localStorage.removeItem('kakao_refresh_token');
+      dispatch(logoutUser());
+      dispatch(setLoginChecked(true));
+    }
   };
 
   return null;
 };
 
-const KakaoLoginButton: React.FC = () => { // 함수 컴포넌트에 대한 타입 정의
+const KakaoLoginButton: React.FC = () => {
   const handleKakaoLogin = async () => {
-    console.log("클릭됨 ");
-    const Rest_api_key= CONST.KAKAO_REST_API_KEY; //REST API KEY
-    const redirect_uri = CONST.KAKAO_REDIRECT_URL //Redirect URI
-    console.log(redirect_uri);
-    // oauth 요청 URL
-    const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`
-    
-    window.location.href = kakaoURL
+    const Rest_api_key = CONST.KAKAO_REST_API_KEY;
+    const redirect_uri = CONST.KAKAO_REDIRECT_URL;
 
-    // const code = new URL(window.location.href).searchParams.get("code");
-  
-    // console.log(code);
+    const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
+    window.location.href = kakaoURL;
   };
+
+  const getAccessToken = async (code: string) => {
+    try {
+      const response = await axios.post(
+        `https://kauth.kakao.com/oauth/token`,
+        null,
+        {
+          params: {
+            grant_type: 'authorization_code',
+            client_id: CONST.KAKAO_REST_API_KEY,
+            redirect_uri: CONST.KAKAO_REDIRECT_URL,
+            code: code,
+          },
+          headers: {
+            'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        const { access_token, refresh_token } = response.data;
+        localStorage.setItem('kakao_token', access_token);
+        localStorage.setItem('kakao_refresh_token', refresh_token);
+        localStorage.setItem('last_login_platform', 'kakao');
+      } else {
+        throw new Error('Failed to get access token');
+      }
+    } catch (error) {
+      console.error('Error getting access token:', error);
+    }
+  };
+
+  useEffect(() => {
+    const url = new URL(window.location.href);
+    const code = url.searchParams.get('code');
+    if (code) {
+      getAccessToken(code);
+    }
+  }, []);
 
   return (
     <Box pb={2}>
-    <img  onClick={handleKakaoLogin} src={kakaoLoginImage} alt="description"/>
-  </Box>
+      <img onClick={handleKakaoLogin} src={kakaoLoginImage} alt="Kakao Login" />
+    </Box>
   );
 };
+
 
 const KakaoLogoutButton: React.FC = () => {
   const [open, setOpen] = useState(false);
@@ -99,18 +162,18 @@ const KakaoLogoutButton: React.FC = () => {
   };
 
   const handleLogout = async () => {
-    const accessToken = localStorage.getItem('kakao_token'); // 저장된 토큰을 사용
+    const accessToken = localStorage.getItem('kakao_token');
     const LOGOUT_API_URL = `${process.env.REACT_APP_API_SERVER_URI}/api/auth/logout-kakao`;
 
     try {
-      // 서버에 로그아웃 요청을 보냄
       const response = await axios.post(LOGOUT_API_URL, { access_token: accessToken });
       if (response.data.success) {
         console.log('서버 로그아웃 성공');
-        localStorage.setItem('kakao_token', ''); // 토큰 초기화
-        localStorage.setItem('last_login_platform', '');
+        localStorage.removeItem('kakao_token');
+        localStorage.removeItem('kakao_refresh_token');
+        localStorage.removeItem('last_login_platform');
 
-        dispatch(logoutUser());        
+        dispatch(logoutUser());
         handleClose();
         alert('정상적으로 로그아웃 되었습니다.');
       } else {
@@ -118,17 +181,13 @@ const KakaoLogoutButton: React.FC = () => {
       }
     } catch (error) {
       if (axios.isAxiosError(error)) {
-        // AxiosError 타입인 경우
         if (error.response && error.response.data && error.response.data.reason && error.response.data.reason.code === -401) {
-          // 에러 코드가 "-401"일 때 로그아웃 처리
           console.log('토큰 만료로 인한 로그아웃 처리');
           dispatch(logoutUser());
         } else {
-          // 다른 에러 메시지를 콘솔에 출력
           console.error('서버 에러:', error.response ? error.response.data : 'No response data');
         }
       } else {
-        // Axios 에러가 아닌 다른 종류의 에러 처리
         console.error('Unexpected error:', error);
       }
     }
@@ -139,10 +198,10 @@ const KakaoLogoutButton: React.FC = () => {
       <Button
         onClick={handleClickOpen}
         sx={{
-          mt: '30px', // margin-top: 30px
+          mt: '30px',
           color: '#666666',
           textDecoration: 'underline',
-          fontSize: 'small'
+          fontSize: 'small',
         }}
       >
         로그아웃
@@ -151,9 +210,7 @@ const KakaoLogoutButton: React.FC = () => {
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>{"로그아웃 하시겠습니까?"}</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            로그아웃을 진행하시면 현재 세션에서 로그아웃됩니다.
-          </DialogContentText>
+          <DialogContentText>로그아웃을 진행하시면 현재 세션에서 로그아웃됩니다.</DialogContentText>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">아니오</Button>
@@ -164,4 +221,4 @@ const KakaoLogoutButton: React.FC = () => {
   );
 };
 
-export {KakaoLoginButton, KakaoLogoutButton, AutoKakaoLoginCheck};
+export { KakaoLoginButton, KakaoLogoutButton, AutoKakaoLoginCheck };
